926d1e0e7ed60d177139e12e6f6dd48d1d58c1d3
Hovanca <jahodovelizatko4@gmail.com>
2022-03-06 19:47:12 +0100

    image compression uploaded

--- recepty_projekt/recepty_list/models.py
+++ recepty_projekt/recepty_list/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from io import BytesIO
+from PIL import Image
+import sys
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
 
@@ -10,11 +14,31 @@ class Ingredient(models.Model):
         return self.name+ ' ' +str(self.grams)
 
 
+
+
 #tu sa nam to ukaze ako v administracii
 class Recept(models.Model):
     name = models.CharField(max_length = 50)
     author = models.CharField(max_length = 50)
-    thumbnail = models.ImageField(upload_to = "recepty/", blank = True)
+
+    thumbnail = models.ImageField(upload_to = "recepty/")
+    #dame si tu funkciu save ktora overrunne saving process
+    def save(self, *args, **kwargs):
+        self.thumbnail = self.compress(self.thumbnail)
+        super(Recept,self).save(*args, **kwargs)
+
+    def compress(self, uploaded_image):
+        im = Image.open(uploaded_image)
+        im_io = BytesIO()
+        im_res = im.resize((1020, 573))
+        im.save(im_io, format = 'JPEG', quality=60)
+        #new_image = File(im_io, name=image.name)
+        im_io.seek(0)
+        new_image = InMemoryUploadedFile(im_io, 'ImageField', "%s.jpg" % uploaded_image.name.split('.')[0],
+                                             'image/jpeg', sys.getsizeof(im_io), None)
+        return new_image
+
+
     cooking_time = models.DurationField()
     portions = models.IntegerField()
 
